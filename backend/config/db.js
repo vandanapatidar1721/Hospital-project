cd6aadab4df0cd9eeb31b7d58c2caa4550a0bfeb
vandanapatidar1721 <patidarvandana857@gmail.com>
2026-07-07 18:20:48 +0530

    last

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
+    console.log('Connecting to MongoDB...',process.env.MONGODB_URI);
     const conn = await mongoose.connect(process.env.MONGODB_URI);
+    // console.log(conn);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB connection error: ${error.message}`);
