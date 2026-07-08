9db8c586b13bb14c614548c57b0593d91454f62d
vandanapatidar1721 <patidarvandana857@gmail.com>
2026-07-08 12:57:11 +0530

    frontend vite url change

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://hospital-project-uojh.onrender.com/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://hospital-project-j3le.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
