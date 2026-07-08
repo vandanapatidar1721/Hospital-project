8bd2e99f4951e92b5ffe00018dbce28efb2752cb
vandanapatidar1721 <patidarvandana857@gmail.com>
2026-07-08 11:03:46 +0530

    last

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,12 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    console.log('Connecting to MongoDB...',process.env.MONGODB_URI);
+    if (!process.env.MONGODB_URI) {
+      throw new Error('MONGODB_URI is not set');
+    }
+
+    console.log('Connecting to MongoDB...');
     const conn = await mongoose.connect(process.env.MONGODB_URI);
-    // console.log(conn);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB connection error: ${error.message}`);
